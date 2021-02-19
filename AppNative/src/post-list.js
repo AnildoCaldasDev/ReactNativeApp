8a1ec3935269b5702ad4cd62bcaecacc5035ee2f
anildk5@hotmail.com <anildk5@hotmail.com>
2021-02-18 21:58:26 -0300

    added list for tests

---
+++ AppNative/src/post-list.js
@@ -0,0 +1,62 @@
+/* eslint-disable prettier/prettier */
+import React, {Component} from 'react';
+import {StyleSheet, ScrollView, View, Text} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+export class PostList extends Component {
+  render() {
+    let posts = [
+      {title: 'React Native', description: 'Desc react'},
+      {title: 'C#', description: 'Desc c#'},
+      {title: 'Java', description: 'Desc java'},
+      {title: 'Sql', description: 'Desc sql'},
+      {title: 'Java', description: 'Desc java'},
+      {title: 'C#', description: 'Desc c#'},
+      {title: 'Python', description: 'Desc python'},
+      {title: 'Java', description: 'Desc java'},
+    ];
+
+    return (
+      <ScrollView style={styles.container}>
+        {posts.map((p) => (
+          <View style={styles.postContainer}>
+            <Text style={styles.postTitle}>{p.title}</Text>
+            <Text style={styles.postDescription}>{p.description}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+  },
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  postContainer: {
+    // alignContent: 'flex-start',
+    // alignItems: 'flex-start',
+    marginHorizontal: 20,
+    marginVertical: 10,
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: '#FFF',
+  },
+  postTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+});
